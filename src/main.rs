651cf02fb3cadd9049948cade7896e5db333369c
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-03-08 16:01:17 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
 struct Sq {
+  u: u8,
+  v: u8,
+  
+  hidden: bool, //should be true by default
   mine: bool,
-  hidden: bool,
-  nearby: u8,
+  
+  nearby: u8
 }
 
 fn main() {
+  let example_mine_sq_u1v1 = Sq{
+    u       = 1, 
+    v       =1,
+    
+    hidden  = true,
+    mine    = true,
+    
+    nearby  = 0 //should be set with a function, after the whole map is generated.
+  }
 }
