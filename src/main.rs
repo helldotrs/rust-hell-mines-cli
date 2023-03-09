792f903d9cb0f5eab965d6923d1d3dad18d71956
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-03-09 08:39:14 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,23 @@ struct Sq {
   hidden: bool, //should be true by default
   mine: bool,
   
-  nearby: u8
+  nearby: u8 //how to mut??
 }
 
 fn main() {
+  
+  let rows = 2;
+  let cols = 2;
+  
+  let mut my_grid = vec![vec![MyStruct { 
+    u: 0, v: 0, //FIXME: auto increase
+    hidden: true, mine: true, //FIXME: set mine with auto function
+    nearby: 0, //FIXME/ADD: changed afterwards, based on nearby
+    }; cols]; rows];
+
+  
+  
+  
   let example_mine_sq_u1v1 = Sq{
     u       = 1, 
     v       =1,
