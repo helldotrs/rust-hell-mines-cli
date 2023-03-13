043259da510f3f545c78dac32597322d28eb123b
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-03-13 15:15:35 +0100

    Update mine-prototype.py

--- src/mine-prototype.py
+++ src/mine-prototype.py
@@ -7,7 +7,15 @@ class GridBit:
         self.hidden = True
         self.mine = random.random() < 0.2
         self.nearby = 0
-
+        
+for v in range(5):
+    for u in range(10):
+        print(u, end="")
+        print(v, end="")
+    print("")
+        
+        
+"""
 bit_u0v0 = GridBit(u=0, v=0)
 bit_u0v1 = GridBit(u=0, v=0)
 bit_u0v2 = GridBit(u=0, v=0)
@@ -19,5 +27,5 @@ bit_u1v2 = GridBit(u=0, v=0)
 bit_u2v0 = GridBit(u=0, v=0)
 bit_u2v1 = GridBit(u=0, v=0)
 bit_u2v2 = GridBit(u=0, v=0)
-
+"""
 
